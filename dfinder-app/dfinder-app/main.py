e4933c70a33e3111fc6dbbe0725219ea386e5f08
freakyfee <freakyfee@proton.me>
2022-07-05 16:51:19 +0200

    FastAPI with SVG

--- dfinder-app/dfinder-app/main.py
+++ dfinder-app/dfinder-app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException
+from typing import List
+from pydantic import BaseModel
 import dfinder
 
 
@@ -11,14 +13,31 @@ app = FastAPI(
     description="API for divisionfinder app",
 )
 
+class Instruction(BaseModel):
+    description: str
+    diagrams: List[str]
+    verbal: List[str]
+
+class Instructions(BaseModel):
+    Instructions: List[Instruction]
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/api/calc-divisions/{divisions}")
+@app.get("/find-divs-svg/{divisions}")
 def calc_divisions(divisions: int):
-    if (divisions>0 and divisions < 101):
-        return {"response" : dfinder.CalcDivisionsHTML(divisions)}
+    if (divisions>0):
+        r_divs =  dfinder.FindDivisionsSVG(divisions)
+        return Instructions(Instructions=[Instruction(description=div.description, diagrams=div.diagrams, verbal=div.verbal) for div in r_divs ])
+    else:
+        raise HTTPException(status_code=403, detail="Please enter a number between 0 and 101 (otherwise you'll overload the server.")
+
+@app.get("/fold-cycle-svg/")
+def fold_cycle(total: int, start: int):
+    if (total  > 0 and total < 101):
+        start = start % total # so that 0 < start <  total
+        cycle = dfinder.FoldCyclesSVG(total, start)
+        return Instruction(description=cycle.description,diagrams=cycle.diagrams,verbal=cycle.verbal)
     else:
         raise HTTPException(status_code=403, detail="Please enter a number between 0 and 101 (otherwise you'll overload the server.")
--- web/src/repositories/ProjectRepository.js
+++ web/src/repositories/ProjectRepository.js
@@ -8,7 +8,7 @@ export default {
   baseURL: `${location.protocol}//${host}:${port}`,
 
   async getDiagrams(divisions) {
-      const resp = await fetch(`${this.baseURL}/api/api/calc-divisions/${divisions}`)
+      const resp = await fetch(`${this.baseURL}/api/calc-divisions/${divisions}`)
       return await resp.json()
   }
 }
